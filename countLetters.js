c0f32df541428d81ccf55a1c41e282f064224126
Jinny Huang <jinny921@gmail.com>
2017-04-05 22:19:01 +0000

    changed formating and name

--- countLetters.js
+++ countLetters.js
@@ -1,28 +1,19 @@
-var letters = process.argv.slice(2).toString().toLowerCase().split(",").join("");
+var letters = process.argv.slice(2).join("");
 
-function getFrequency(string) {
-    var freq = {};
-    for (var i=0; i<string.length;i++) {
-        var character = string.charAt(i);
-        // console.log(character[i]);
-        if (freq[character]) {
-           freq[character]++;
-        } else {
-           freq[character] = 1;
-        }
-    }
-        // console.log(character[i]);
-    return freq;
+function countLetters(string) {
+  var freq = {};
+  for (var i=0; i<string.length;i++) {
+      var character = string.charAt(i).toLowerCase();
+      if (freq[character]) {
+          freq[character]++;
+      } else {
+          freq[character] = 1;
+      }
+  }
+
+  return freq;
 };
 
-// for(i = 0; i < letters.length; i++) {
-//     var str = letters;
-//     console.log(str[i]);
-// }
 
-// for (var x=0; x < s.length; ++x) {
-// console.log(s[x]+'\n');}
-// for (i = 0; i <letters.length; i++) {
-//     console.log(getFrequency(letters + '\n'));
-// }
-console.log(getFrequency(letters));
+
+console.log(countLetters(letters));
